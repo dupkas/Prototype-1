6a96c30497a3bafc89c35837c39c74974f01cedb
dupkas <dvgerus@gmail.com>
2022-01-23 12:52:23 +0200

    3 Camera views mode

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject NormalCam;
+    public GameObject FPCam;
+    public GameObject FPCam2;
+    public int CamMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (CamMode == 2)
+            {
+                CamMode = 0;
+            }
+            else
+            {
+                CamMode += 1;
+            }
+            StartCoroutine(ModeChange ());
+        }
+    }
+    
+    IEnumerator ModeChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (CamMode == 0)
+        {
+            NormalCam.SetActive(true);
+            FPCam.SetActive(false);
+        }
+        if (CamMode == 1)
+        {
+            NormalCam.SetActive(false);
+            FPCam.SetActive(true);
+        }
+        if (CamMode == 2)
+        {
+            FPCam2.SetActive(true);
+            FPCam.SetActive(false);
+        }
+    }
+}
